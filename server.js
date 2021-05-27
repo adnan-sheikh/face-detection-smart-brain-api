c1da50df7a9a0acb95bc66aa6ba5a40f0335f56c
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-27 21:17:30 +0530

    finalize all endpoints

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = knex({
   connection: {
     host: '127.0.0.1',
     user: 'postgres',
-    password: 'adnan',
+    password: '',
     database: 'smartbrain',
   },
 });
@@ -16,27 +16,6 @@ const db = knex({
 // db.select('*').from('users').then(console.log);
 
 const app = express();
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0, // to track number of images submitted for detection
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0, // to track number of images submitted for detection
-      joined: new Date(),
-    },
-  ],
-};
 
 app.use(express.json()); // so that body in post request is parsed
 app.use(cors());
@@ -46,31 +25,57 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password == database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('Error logging in.');
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('user not found please register'));
+      } else {
+        res.status(400).json('Wrong credentials');
+      }
+    })
+    .catch(err => res.status(400).json('Please register and then sign in.'));
 });
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) =>
-      res.status(400).json('This user is already registered, try signing in!')
-    );
+  const hash = bcrypt.hashSync(password);
+
+  // bcrypt.compareSync("bacon", hash); // true
+  // bcrypt.compareSync("veggies", hash); // false
+
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  })
+  .catch((err) =>
+    res.status(400).json('This user is already registered, try signing in!')
+  );
 });
 
 app.get('/profile/:id', (req, res) => {
@@ -85,22 +90,25 @@ app.get('/profile/:id', (req, res) => {
         res.status(400).json('user not found');
       }
     })
-    .catch((err) => res.status(400).json('There is an error in getting the user'));
+    .catch((err) =>
+      res.status(400).json('There is an error in getting the user')
+    );
 });
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json('user not found');
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      if (entries.length) {
+        res.json(entries[0]);
+      } else {
+        res.status(404).json('user not found');
+      }
+    })
+    .catch((err) => res.status(400).json('Unable to get entries'));
 });
 
 app.listen(3000, () => {
